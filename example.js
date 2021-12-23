052580214c9b3e3896f40cc50ea91140d21a4ad9
YeFei <316606233@qq.com>
2021-12-23 11:02:28 +0800

    2.3 模块加载标准

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 const { Core } = require('@zenweb/core');
-const { setup } = require('./dist/index');
+const { default: meta } = require('./dist/index');
 
 const app = new Core();
-app.setup(setup);
+app.setup(meta());
 app.start();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import * as Koa from 'koa';
-import { Core } from '@zenweb/core';
+import { SetupFunction } from '@zenweb/core';
 
 const START_TIME = Symbol('zenweb-meta#startTime');
 
@@ -8,29 +7,35 @@ export interface MetaOption {
   showProcessTime: boolean;
 }
 
-export async function setup(core: Core, opt?: MetaOption) {
+export default function setup(opt?: MetaOption): SetupFunction {
   opt = Object.assign({
     showVersion: true,
     showProcessTime: true,
   }, opt);
   if (opt.showVersion) {
-    var version: string = require('zenweb/package.json').version;
-    console.log('zenweb version:', version);
-  }
-  Object.defineProperty(core.koa.context, 'startTime', { get() { return this[START_TIME]; } });
-  core.use(async function meta(ctx: Koa.Context, next: Koa.Next) {
-    ctx[START_TIME] = Date.now();
-    if (opt.showVersion) {
-      ctx.set('X-Powered-By', `zenweb/${version || '-'} node/${process.version}`);
-    }
     try {
-      await next();
-    } finally {
-      if (opt.showProcessTime) {
-        ctx.set('X-Process-Time', (Date.now() - ctx[START_TIME]).toString());
-      }
+      var version: string = require('zenweb/package.json').version;
+      console.log('zenweb version:', version);
+    } catch (err) {
+      console.warn('load zenweb version error:', err);
     }
-  });
+  }
+  return async function meta(setup) {
+    setup.defineContextProperty('startTime', { get() { return this[START_TIME]; } });
+    setup.middleware(async function metaHeader(ctx, next) {
+      ctx[START_TIME] = Date.now();
+      if (opt.showVersion) {
+        ctx.set('X-Powered-By', `zenweb/${version || '-'} node/${process.version}`);
+      }
+      try {
+        await next();
+      } finally {
+        if (opt.showProcessTime) {
+          ctx.set('X-Process-Time', (Date.now() - ctx[START_TIME]).toString());
+        }
+      }
+    });
+  }
 }
 
 declare module 'koa' {
